e396e55ea66e95f228fd6f1b85c8295ed13e00f3
hema <hemapn2@gmail.com>
2025-05-21 16:07:04 +0530

    react-native added

---
+++ src/welcome.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+const welcome = () => {
+    return(
+        <View  >
+            <Text style={styles.red} >welcome all guys!</Text>
+        </View>
+    );
+};
+
+const styles=StyleSheet.create({
+    red:{
+        color:'red'
+ }
+});
+export default welcome;
